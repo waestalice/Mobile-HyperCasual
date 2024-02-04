aacffa60f4c1767ee5eeadc457ece6efcc97767a
waestalice <alice.barreto890@gmail.com>
2024-02-04 19:21:17 -0300

    add power ups in game

--- Assets/Coin/ItemCollectableCoin.cs
+++ Assets/Coin/ItemCollectableCoin.cs
@@ -4,12 +4,40 @@ using UnityEngine;
 
 public class ItemCollectableCoin :  ItemCollectableBase
 {
-	public Collider2D Itemcollider;
+	public Collider collider;
+	public bool collect = false;
+	public float lerp = 5f;
+	public float minDistance = 1f;
+
+	private void Start()
+	{
+		//CoinsAnimationManager.Instance.RegisterCoin(this);
+	}
 
 	protected override void OnCollect()
 	{
-		base.OnCollect();
-		ItemManager.Instance.AddCoins();
-		Itemcollider.enabled = false;
+		 base.OnCollect();
+		 collider.enabled = false;
+		 collect = true;
+		 //PlayerController.Instance.Bounce();
+	}
+
+	protected override void Collect()
+	{
+		OnCollect();
+	}
+
+	private void Update()
+	{
+		if(collect)
+		{
+			transform.position = Vector3.Lerp(transform.position, PlayerController.Instance.transform.position, lerp * Time.deltaTime);
+
+			if(Vector3.Distance(transform.position, PlayerController.Instance.transform.position) < minDistance)
+			{
+				 //HideItens();
+				 Destroy(gameObject);
+			}
+		}
 	}
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using EBAC.Core.Singleton;
+using DG.Tweening;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : Singleton<PlayerController>
 {
     //publics
     [Header("Lerp")]
@@ -16,9 +19,30 @@ public class PlayerController : MonoBehaviour
 
     public GameObject endScreen;
 
+    [Header("Coin Setup")]
+    public GameObject coinCollector;
+
+    [Header("TextMeshPro")]
+    public TextMeshPro uiTextPowerUp;
+
+    [Header("Limits")]
+    public float limit = 4;
+    public Vector2 limitVector = new Vector2(-4, 4);
+
+
+    public bool invencible = false;
+
     //privates
     private bool _canRun;
     private Vector3 _pos;
+    private float _currentSpeed;
+    private Vector3 _startPosition;
+    
+    private void Start()
+    {
+        _startPosition = transform.position;
+        ResetSpeed();
+    }
 
     // Update is called once per frame
     void Update()
@@ -30,14 +54,14 @@ public class PlayerController : MonoBehaviour
         _pos.z = transform.position.z;
 
         transform.position = Vector3.Lerp(transform.position, _pos, lerpSpeed * Time.deltaTime);
-        transform.Translate(transform.forward * speed * Time.deltaTime);
+        transform.Translate(transform.forward * _currentSpeed * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.transform.tag == tagToCheckEnemy)
         {
-            EndGame();
+            if(!invencible) EndGame();
         }
     }
 
@@ -45,7 +69,7 @@ public class PlayerController : MonoBehaviour
     {
         if(other.transform.tag == tagToCheckEndLine)
         {
-            EndGame();
+            if(!invencible) EndGame();
         }
     }
 
@@ -59,4 +83,46 @@ public class PlayerController : MonoBehaviour
     {
         _canRun = true;
     }
+
+    #region POWER UPS
+    public void SetPowerUpText(string s)
+    {
+        uiTextPowerUp.text = s;
+    }
+
+    public void PowerUpSpeedUp(float f)
+    {
+        _currentSpeed = f;
+    }
+    
+    public void ResetSpeed()
+    {
+        _currentSpeed = speed;
+    }
+     
+    public void SetInvencible(bool b = true)
+    {
+        invencible = b;
+    }
+
+    public void ChangeHeight(float amount, float duration, float animationDuration, Ease ease)
+    {
+         /*var p = transform.position;
+         p.y = _startPosition.y + amount;
+         transform.position = p;*/
+
+         transform.DOMoveY(_startPosition.y + amount, animationDuration).SetEase(ease);//.OnComplete(ResetHeight);a
+         Invoke(nameof(ResetHeight), duration);
+    }
+
+    public void ResetHeight()
+    {
+         transform.DOMoveY(_startPosition.y, .1f);
+    }
+
+    public void ChangeCoinCollectorSize(float amount)
+    {
+        coinCollector.transform.localScale = Vector3.one * amount;
+    }
+    #endregion
 }
