65892dc1dc25eb6d921daf3042d2ddfecc621369
waestalice <alice.barreto890@gmail.com>
2024-02-12 19:10:04 -0300

    Add Coins Animationo

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class LevelManager : MonoBehaviour
 {
@@ -18,6 +19,11 @@ public class LevelManager : MonoBehaviour
 
    [SerializeField] private List<LevelPieceBase> _spawnedPieces = new List<LevelPieceBase>();
    private LevelPieceBasedSetup _currSetup;
+
+   [Header("Animation")]
+   public float scaleDuration = .2f;
+   public float scaleTimeBetweenPieces = .1f;
+   public Ease ease = Ease.OutBack;
 
    private void Start()
    {
@@ -81,6 +87,25 @@ public class LevelManager : MonoBehaviour
 		  }
 
 		  ColorManager.Instance.ChangeColorByType(_currSetup.artType);
+
+		  StartCoroutine(ScalePiecesByTime());
+   }
+
+   IEnumerator ScalePiecesByTime()
+   {
+	   foreach(var p in _spawnedPieces)
+	   {
+		   p.transform.localScale = Vector3.zero;
+	   }
+
+	   yield return null;
+
+	   for(int i = 0; i < _spawnedPieces.Count; i++)
+	   {
+		   _spawnedPieces[i].transform.DOScale(1, scaleDuration).SetEase(ease);
+		   yield return new WaitForSeconds(scaleTimeBetweenPieces);
+	   }
+	   CoinsAnimationManager.Instance.StartAnimations();
    }
 
    private void CreateLevelPiece(List<LevelPieceBase> list)
